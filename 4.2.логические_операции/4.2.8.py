ce8ccc06d745df19c5ed2ab011774e3fcb9feb33
aleksandr.shaydullin <aleksandr.shaydullin@lamoda.ru>
2023-01-14 19:25:23 +0400

    Rename all file
    - feat: Переименовал все файлы в соответствии с соглашением.

---
+++ 4.2.логические_операции/4.2.8.py
@@ -0,0 +1,5 @@
+a = int(input())
+if a > -1 and a < 17:
+    print('Принадлежит')
+else:
+    print('Не принадлежит')
---
+++ 4.2.логические_операции/4.2.9.py
@@ -0,0 +1,5 @@
+n = int(input())
+if not (-3 < n < 7):
+    print('Принадлежит')
+else:
+    print('Не принадлежит')
