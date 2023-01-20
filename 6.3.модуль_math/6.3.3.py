30952af0b0b7614d8b04589ade6b1da751fa418b
aleksandr.shaydullin <aleksandr.shaydullin@lamoda.ru>
2023-01-20 18:44:36 +0400

    ADDED VERY MANY ITEMS!!!
    - BREAKING CHANGES: добавил супер-миллион файлов!

---
+++ 6.3.модуль_math/6.3.3.py
@@ -0,0 +1,5 @@
+from math import pi
+krug = float(input())
+place_krug = pi * (krug ** 2)
+line_krug = 2 * pi * krug
+print(place_krug, line_krug, sep="\n")
---
+++ 6.3.модуль_math/6.3.5.py
@@ -0,0 +1,4 @@
+from math import *
+numx = radians(float(input()))
+result = sin(numx) + cos(numx) + tan(numx) ** 2
+print(result)
